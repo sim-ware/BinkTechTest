db7d36acea76a67a4db19a90da5ed506b1352513
Samir Gossain <samir.gossain@gmail.com>
2017-06-20 14:53:09 +0100

    Noun.erb returns selected colour and randomised noun

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ end
 
 post '/colour' do
   @colour = params[:colour]
+  @noun = ['Airline', 'Bird', 'Book', 'Building', 'Cat', 'Country', 'Fear', 'Happiness', 'Man', 'Mountain', 'Ocean', 'Phone', 'Pride', 'Religion', 'Rice', 'Snow', 'State', 'Water', 'Wealth'].sample
   puts params
   erb :noun
 end
