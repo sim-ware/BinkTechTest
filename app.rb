d26d3e7eb1080bf6ed89ab1511c1e5754f33a72f
Samir Gossain <samir.gossain@gmail.com>
2017-06-21 09:43:22 +0100

    Refactors, removes unfinished views and code from app.rb

--- app.rb
+++ app.rb
@@ -11,20 +11,11 @@ require './lib/searchlog'
   before do
      @search = Search.new
      @search.random_noun
-    #  @search_log = Searchlog.new
   end
 
   post '/colour' do
     @search.colour_selector(params[:colour])
     @search.make_filter
     @search.return_search
-    # a = @search.compact
-    # @search_log.add_search(a)
-    # puts a
     erb :results
   end
-
-  # post '/history' do
-  #   "Hello World"
-  #   erb :history
-  # end
